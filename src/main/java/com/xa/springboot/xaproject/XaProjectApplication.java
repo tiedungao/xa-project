d1dce371245674cb62276fd83ad5b068ea5379d7
tiedungao <gaotiedun@outlook.com>
2019-10-10 20:47:30 +0800

    first commit;

---
+++ src/main/java/com/xa/springboot/xaproject/XaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.xa.springboot.xaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XaProjectApplication.class, args);
+    }
+
+}
